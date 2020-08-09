109ee6074916b7a4707349f1fde2b1b9f2ea7d0f
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-08-09 14:58:43 +0200

    Implement Heap

--- src/main/java/Heap.java
+++ src/main/java/Heap.java
@@ -1,2 +1,103 @@
+import java.util.Arrays;
+
 public class Heap {
+    private int capacity = 10;
+    private int size = 0;
+
+    int[] items = new int[capacity];
+
+    private int getLeftChildIndex(int parentIndex) {
+        return 2*parentIndex+1;
+    }
+
+    private int getRightChildIndex(int parentIndex){
+        return 2*parentIndex+2;
+    }
+
+    private int getParentIndex(int childIndex){
+        return (childIndex - 1)/2;
+    }
+
+    private boolean hasLeftChild(int index){
+        return getLeftChildIndex(index) < size;
+    }
+
+    private boolean hasRightChild(int index){
+        return getRightChildIndex(index) < size;
+    }
+
+    private boolean hasParent(int index){
+        return getParentIndex(index) < size;
+    }
+
+    private int leftChild(int index){
+        return items[getLeftChildIndex(index)];
+    }
+
+    private int rightChild(int index){
+        return items[getRightChildIndex(index)];
+    }
+
+    private int parent(int index){
+        return items[getParentIndex(index)];
+    }
+
+    private void swap(int indexOne, int indexTwo){
+        int temp = items[indexOne];
+        items[indexOne] = items[indexTwo];
+        items[indexTwo] = temp;
+    }
+
+    private void ensureExtraCapacity(){
+        if(size == capacity){
+            items = Arrays.copyOf(items, capacity*2);
+            capacity*=2;
+        }
+    }
+
+    public int peek(){
+        if(size == 0) throw new IllegalStateException();
+        return items[0];
+    }
+
+    public int poll(){
+        if (size == 0) throw new IllegalStateException();
+        int item = items[0];
+        items[0] = items[size - 1];
+        size--;
+        heapifyDown();
+        return item;
+    }
+
+    private void heapifyDown() {
+        int index = 0;
+        while(hasLeftChild(index)){
+            int smallerChildIndex = getLeftChildIndex(index);
+            if(hasRightChild(index) && rightChild(index) < smallerChildIndex ){
+                smallerChildIndex = getRightChildIndex(index);
+            }
+
+            if(items[index] < items[smallerChildIndex]){
+                break;
+            }else{
+                swap(index, smallerChildIndex);
+            }
+            index = smallerChildIndex;
+        }
+    }
+
+    public void add(int item){
+        ensureExtraCapacity();
+        items[size] = item;
+        size++;
+        heapifyUp();
+    }
+
+    private void heapifyUp() {
+        int index = size - 1;
+        while(hasParent(index) && parent(index) > items[index]){
+            swap(index, getParentIndex(index));
+            index = getParentIndex(index);
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+    public static void main(String[] args) {
+        Heap mainHeap = new Heap();
+
+        mainHeap.add(15);
+        mainHeap.add(17);
+        mainHeap.add(20);
+        mainHeap.add(10);
+
+        mainHeap.poll();
+
+        System.out.println(mainHeap.peek());
+
+        mainHeap.add(8);
+
+        System.out.println(mainHeap.peek());
+    }
 }
